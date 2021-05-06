514fc997dad7255541e7abf8d32840a4a86d2d46
Nicholas <nbaddorf@gmail.com>
2021-05-06 14:25:17 -0400

    working on auto tool chainger code

--- Ender3CustomCNC.py
+++ Ender3CustomCNC.py
@@ -1,15 +1,11 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog
 from pathlib import Path
 import shutil
 import os
 import re
 
-
-fileFound=False
-targetFileDir = "/"
-homeDir = str(Path.home())
-printHeight = 20
 penOffsetZ = 2 #pen is 2mm below nozzle
 penOffsetX = -43
 penOffsetY = -26
@@ -17,9 +13,36 @@ printerMaxX = 235
 printerMaxY = 235
 
 
+fileFound=False
+targetFileDir = "/"
+homeDir = str(Path.home())
+printHeight = 20
+toolChaingeState = True
+
+def toolChaingeSwitch():
+    global toolChaingeState
+    if toolChaingeState:
+        tool_button.config(image = off)
+        print("off")
+        toolChaingeState = False
+    else:  
+        tool_button.config(image = on)
+        print("on")
+        toolChaingeState = True
+        
   
 # Function for opening the
 # file explorer window
+
+def saveToolCode():
+    txt = open("StartToolChaingeCode.txt", "w+")
+    txt.write(StartToolChaingeCode.get(1.0, "end-1c"))
+    txt.close()
+
+    txt = open("EndToolChaingeCode.txt", "w+")
+    txt.write(EndToolChaingeCode.get(1.0, "end-1c"))
+    txt.close()
+
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir = homeDir, title = "Select File",filetypes = (("all files","*.*"),("NC files","*.nc")))
     label_file_explorer.configure(text="File Opened: "+filename, fg="white")
@@ -51,6 +74,7 @@ def runProgram():
     global fileFound
     global targetFileDir
     global printHeight
+    global toolChaingeState
     if fileFound:
         stillGoodToGo=True
         dirname, fname = os.path.split(targetFileDir)
@@ -60,6 +84,8 @@ def runProgram():
         f = open(dirname + "/" + curntName + ".gcode")
         textF = f.read()
         textF = str("G28\nM420 S1\nG29\n") + textF
+        if toolChaingeState:
+            re.sub('G90\n', str(StartToolChaingeCode.get(1.0, "end-1c")) + "\n", textF)
         #print(textF)
         printHeight = sli1.get()
         if(edit_string(dirname + "/" + curntName + ".gcode")):
@@ -103,51 +129,77 @@ def runProgram():
 # Create the root window
 window = Tk()
   
-
+on = PhotoImage(file = "on.png")
+off = PhotoImage(file = "off.png")
 
 # Set window title
 window.title('Nicks Ender 3 CNC Customizer')
   
 # Set window size
 window.geometry("800x500")
-  
+
 #Set window background color
 #window.config(background = "white")
 # Create a File Explorer label
-descriptionLable = Label(window,
-                            text = "To use this program, export a toolpath from easel with:\nPlunge Rate: 50mm/m\nDepth per pass: 1mm\nMaterial size: 235mm by 235mm by 1mm\n",
-                            width = 100, height = 4,
-                            fg = "white")
 
-label_file_explorer = Label(window,
-                            text = "",
-                            width = 100, height = 4,
-                            fg = "white")
+
+
+#frame = Frame(window,
+##        border=1,
+ #       relief=GROOVE,
+ #       background="blue",
+ #   )
 
 sli1 = Scale(window, from_=50, to=0, tickinterval=0.1, orient=VERTICAL, bg = "RED")
-sli1.place(relx=0.15,rely=0.06,relwidth=0.05, relheight=0.8)
+sli1.grid(pady=10,padx=5, sticky=S+N, rowspan=10)
 
-  
-      
-button_explore = Button(window,
-                        text = "Browse Files",
-                        command = browseFiles, bg="white")
-  
-button_run = Button(window,
-                     text = "Run",
-                     command = runProgram, bg="white")
-  
-# Grid method is chosen for placing
-# the widgets at respective positions
-# in a table like structure by
-# specifying rows and columns
-descriptionLable.grid(column = 1, row = 1)
-label_file_explorer.grid(column = 1, row = 2)
-  
-button_explore.grid(column = 1, row = 3)
-#sli.grid(column = 1, row = 4)
+#toolChaingeCodeText = StringVar()
+StartToolChaingeLabel = Label(window, text="Begining tool chainge code")
+StartToolChaingeLabel.grid(column=1,row=0, sticky=S, pady=10)
 
-button_run.grid(column = 1,row = 4)
+StartToolChaingeCode = ScrolledText(window, width = 15, height=7)
+StartToolChaingeCode.grid(column=1,row=1, sticky=N, rowspan=2)
+
+EndToolChaingeLabel = Label(window, text="End tool chainge code")
+EndToolChaingeLabel.grid(column=1,row=3, sticky=NS)
+
+EndToolChaingeCode = ScrolledText(window, width = 15, height=7)
+EndToolChaingeCode.grid(column=1,row=4, sticky=N, rowspan=2)
+
+toolSave = Button(window, text="Save Tool Chainge", command=saveToolCode)
+toolSave.grid(column=1,row=6)
+
+descriptionLable = Label(window,text = "To use this program, export a toolpath from easel with:\nPlunge Rate: 50mm/m\nDepth per pass: 1mm\nMaterial size: 235mm by 235mm by 1mm\n",fg = "white")
+descriptionLable.grid(column = 2, row = 0, pady=10)
+
+label_file_explorer = Label(window,text = "",fg = "white")
+label_file_explorer.grid(column = 2, row = 1)
+
+button_explore = Button(window,text = "Browse Files",command = browseFiles, bg="white")
+button_explore.grid(column = 2, row = 2)
+
+toolLable = Label(window,text = "Use Auto Toolchainger",fg = "white")
+toolLable.grid(column=2,row=3)
+
+tool_button = Button(window, image = on, bd = 0, command = toolChaingeSwitch)
+tool_button.grid(column = 2, row = 4, pady=2)
+
+button_run = Button(window,text = "Run",command = runProgram, bg="white")
+button_run.grid(column = 2,row = 5)
   
+print("Starting Program...")
+try:
+    txt = open("StartToolChaingeCode.txt", "r")
+    StartToolChaingeCode.insert(1.0, txt.read())
+    txt.close()
+except:
+    print("No Start Tool chainge code file to load")
+try:
+    txt = open("EndToolChaingeCode.txt", "r")
+    EndToolChaingeCode.insert(1.0, txt.read())
+    txt.close()
+except:
+    print("No End Tool chainge code file to load")
+toolChaingeSwitch()
 # Let the window wait for any events
 window.mainloop()
